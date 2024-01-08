04d0b640acfc17b93036a7fc9bd112e51995c1c5
Ankitha023 <72785387+Ankitha023@users.noreply.github.com>
2024-01-08 11:42:14 -0600

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -1,3 +1,7 @@
+#Input: nums = [2,7,11,15], target = 9
+#Output: [0,1]
+#You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         # for i in range(len(nums)):
